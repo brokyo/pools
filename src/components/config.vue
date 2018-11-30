63be728d3bd05f6c7ad6caea68283a400605bc9b
brokyo <alexcarusillo@gmail.com>
2018-11-29 20:17:51 -0500

    moving to own repo

---
+++ src/components/config.vue
@@ -0,0 +1,214 @@
+<template>
+	<main>
+      <div v-for="(system, index) in systems">
+        <h3>system {{index}} - {{(system.params.timing.duration).toFixed()}} Seconds</h3>
+        <div class="params">
+          <div class="category">
+            <h4>Timing</h4>
+            <p>In Seconds. Set start time and gap in between system looping.</p>
+            <div class="input">
+              <label>Start [seconds]</label>
+              <input type="number" v-model.number="system.params.timing.start"></input>
+            </div>
+            <div class="input">
+              <label>Interval [seconds]</label>
+              <input type="number" v-model.number="system.params.timing.interval"></input>
+            </div>
+          </div>
+
+          <div class="category">
+            <h4>Playback</h4>
+            <p>Set system playback speed. 1 is normal, 2 is double, 0.5 is half-speed, etc</p>
+            <div class="input">
+              <label>Playback Rate Base [decimal]</label>
+              <input type="number" v-model.number="system.params.playback.base" @change="changeDuration(system)"></input>
+            </div>
+            <div class="input">
+              <label>Playback Rate range [decimal %]</label>
+              <input type="number" v-model.number="system.params.playback.range"></input>
+            </div>
+            <div class="input">
+              <label>New Values After [loops]</label>
+              <input type="number" v-model.number="system.params.playback.interval"></input>
+            </div>
+          </div>
+
+          <div class="category">
+            <h4>Note</h4>
+            <p>Max percentange (as decimal) note pitch will uniformly change.</p>
+            <div class="input">
+              <label>Note Smudge Base [decimal]</label>
+              <input type="number" v-model.number="system.params.note.base"></input>
+            </div>
+            <div class="input">
+              <label>Smudge Range [decimal]</label>
+              <input type="number" v-model.number="system.params.note.range"></input>
+            </div>
+            <div class="input">
+              <label>New Values After [loops]</label>
+              <input type="number" v-model.number="system.params.note.interval"></input>
+            </div>
+          </div>
+
+          <div class="category">
+            <h4>Duration</h4>
+            <p>Max percentage (as decimal) note duration will uniformly change.</p>
+            <div class="input">
+              <label>Duration Smudge Base [decimal]</label>
+              <input type="number" v-model.number="system.params.duration.base"></input>
+            </div>
+            <div class="input">
+              <label>Duration Smudge Range [decimal]</label>
+              <input type="number" v-model.number="system.params.duration.range"></input>
+            </div>
+            <div class="input">
+              <label>New Values After [loops]</label>
+              <input type="number" v-model.number="system.params.duration.interval"></input>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="canvas">
+      </div>
+      <pre v-for="system in systems">
+        {{system}}
+      </pre>
+	</main>
+</template>
+
+<script>
+var Tone = require('tone')
+var p5 = require('p5')
+var _ = require('lodash')
+
+export default {
+  name: 'config',
+  props: ['systems'],
+  data () {
+  	return {
+      p5Objects: []
+  	}
+  },
+  methods: {
+  	getMidiLength(systems) {
+  		let systemMidiMin = []
+  		let systemMidiMax = []
+
+  		systems.forEach((system) => {
+  			systemMidiMin.push(_.minBy(system.seeds, 'midi').midi)
+	   		systemMidiMax.push(_.maxBy(system.seeds, 'midi').midi)
+		  })
+
+  		let max = _.max(systemMidiMax) + 3
+  		let min = _.min(systemMidiMin) - 3
+  		return {range: max - min , max: max}
+  	},
+    changeDuration(system) {
+      system.active.playbackRate = system.params.playback.base
+      system.active.duration /= system.active.playbackRate
+    }
+  },
+  mounted () {
+  	var vue = this
+
+  	let midiLength = this.getMidiLength(this.systems)
+
+		// Vue values
+  	var canvasWidth = 1200
+  	var canvasHeight = 200
+
+    var systemDuration = (this.systems[0].active.duration * 0.75) + (this.systems[1].active.duration * 0.75)
+  	var canvasPixPerSecond = canvasWidth / systemDuration
+    var canvasPixPerSystemHeight = canvasHeight / this.systems.length
+
+  	var pixPerNote = canvasPixPerSystemHeight / midiLength.range
+
+
+    function System(system) {
+      // Commented out need to be in display function or they wont update
+      // this.x = system.start * canvasPixPerSecond
+      this.y = system.index * canvasPixPerSystemHeight
+      // this.width = system.length * canvasPixPerSecond
+      this.height = canvasPixPerSystemHeight
+
+      this.display = function(sketch) {
+        sketch.rect(system.params.timing.start * canvasPixPerSecond, this.y, system.active.duration * canvasPixPerSecond, this.height)
+      }
+
+      this.drawSeeds = function(sketch) {
+        system.seeds.forEach((seed) => {
+          sketch.rect(
+            ((seed.start / system.params.playback.base) + system.params.timing.start) * canvasPixPerSecond,
+            ((midiLength.max - seed.midi) * pixPerNote) + (system.index * canvasPixPerSystemHeight),
+            (seed.duration / system.params.playback.base) * canvasPixPerSecond,
+            pixPerNote
+          )
+        })
+      }
+
+      this.drawEcho = function(sketch) {
+        system.seeds.forEach((seed) => {
+          for (var i = 1; i < system.params.patch.echo; i++) {
+            sketch.fill('rgba(65, 65, 65,' + (1 - (i/system.params.patch.echo)) + ')')
+            sketch.rect((seed.start / system.params.playback.base * ((system.active.duration * canvasPixPerSecond) / system.active.duration)) + (system.params.timing.start / system.params.playback.base * ((system.active.duration * canvasPixPerSecond) / system.active.duration)) + ((5 * i) * ((system.active.duration * canvasPixPerSecond) / system.active.duration)), ((midiLength.max - seed.midi) * pixPerNote) + (system.index * canvasPixPerSystemHeight), seed.duration / system.params.playback.base * ((system.active.duration * canvasPixPerSecond) / system.active.duration), pixPerNote)
+          }
+        })
+      }
+
+    }
+
+    this.systems.forEach((system) => {
+      this.p5Objects.push(new System(system))
+    })
+
+  	// P5 setup
+  	var myp5 = new p5((sketch) => {
+  	  sketch.setup = () => {
+  	  	let canvas = sketch.createCanvas(canvasWidth, canvasHeight);
+  	    canvas.parent('canvas')
+  	    sketch.background(0)
+  	  };
+
+  	  sketch.draw = () => {
+        sketch.background(0)
+        this.p5Objects.forEach((system)=>{
+          sketch.fill(255)
+          system.display(sketch)
+          sketch.fill(25)
+          system.drawSeeds(sketch)
+          system.drawEcho(sketch)
+        })
+      }
+  	})
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .params {
+    display: flex;
+    width: 1000px;
+  }
+
+  label {
+    display: block;
+    font-size: 12px;
+    font-weight: 900;
+  }
+  .category {
+    flex-grow: 1;
+
+    h4 {
+      margin-bottom: 4px;
+    }
+
+    p {
+      font-size: 10px;
+      width: 120px;
+    }
+  }
+
+  .input {
+
+  }
+</style>
---
+++ src/components/play.vue
@@ -0,0 +1,133 @@
+<template>
+  <main>
+    <button v-if="!playing" @click="startSystem">Play</button>
+    <img :src="embed"></img>
+  </main>
+</template>
+
+<script>
+  var Tone = require('tone')
+
+  export default {
+    name: 'play',
+    props: ['systems'],
+    data() {
+      return {
+        cacheBreak: 0,
+        possibleCams: ['http://207.251.86.238/cctv391.jpg', 'http://206.140.121.226/jpg/image.jpg', 'http://128.151.213.230/jpg/image.jpg', 'http://airportcam.puc.edu/-wvhttp-01-/GetStillImage?camera_id=1', 'http://82.144.57.103/oneshotimage.jpg', 'http://pcc.miemasu.net/snapshotJPEG?Resolution=640x480&amp;Quality=Clarity', 'http://ds4ams.viewnetcam.com/snapshotJPEG?resolution=320x240'],
+        playing: false
+      }
+    },
+    computed: {
+      embed() {
+        return 'http://207.251.86.238/cctv680.jpg?cache=' + this.cacheBreak
+        // return 'http://206.140.121.226/jpg/image.jpg?cache=' + this.cacheBreak
+      }
+    },
+    methods: {
+      startSystem(system) {
+        this.playing = true
+        this.systems.forEach((system) => {
+          setTimeout(() => this.loopSystem(system), system.params.timing.start * 1000)
+        })
+      },
+      loopSystem(system) {
+        console.log('system', system.index, 'loop')
+        // Temporary (wind blowing leaves) - reset to seeds every loop
+        system.nextEventsBase = _.cloneDeep(system.seeds)
+
+        // Semi-permanent (snow on a car windshield) let event changes compound then after x loops move back to seed
+
+        // For probability (puddles near the curb) apply changes only to one note
+
+        // Change Playback
+        if (system.active.loops % system.params.playback.interval === 0) {
+          this.changePlaybackRate(system)
+          console.log('system', system.index, 'change: playback', system.active.playbackRate )
+        }
+
+        // Change Frequency
+        if (system.active.loops % system.params.note.interval === 0) {
+          this.changeFrequencyPercent(system)
+          console.log('system', system.index, 'change: frequency', system.active.noteSmudge )
+        }
+
+        // Change Duration
+        if (system.active.loops % system.params.duration.interval === 0) {
+          this.changeDurationPercent(system)
+          console.log('system', system.index, 'change: duration', system.active.durationSmudge )
+        }
+
+        // Tracks additional start delay time based on duration smudge
+        let compoundSmudge, changedDuration
+        compoundSmudge = 0
+
+        // Select new notes
+        system.nextEventsBase.forEach((event, index) => {
+          let smudgeAmount, newDuration
+          smudgeAmount = event.duration * (1 - system.active.durationSmudge)
+          compoundSmudge += smudgeAmount
+
+          event.frequency *= (system.active.playbackRate * system.active.playbackRate)
+          event.duration += (smudgeAmount / system.active.playbackRate)
+          event.start += (compoundSmudge / system.active.playbackRate)
+
+          this.scheduleEvent(event, system.index)
+
+        })
+
+        // Update Loop
+        let loopInterval = (system.active.duration * 1000) + (system.params.timing.interval * 1000)
+        setTimeout(this.loopSystem, loopInterval, system)
+        system.active.loops++
+      },
+      changePlaybackRate (system) {
+        system.active.playbackRate = system.params.playback.base * (1 + _.round(_.random(-system.params.playback.range, system.params.playback.range), 2))
+        system.active.duration = system.params.timing.duration / system.active.playbackRate
+      },
+      changeFrequencyPercent (system) {
+        system.active.noteSmudge = system.params.note.base * (1 + _.round(_.random(-system.params.note.smudge, system.params.note.smudge), 2))
+      },
+      changeDurationPercent (system) {
+        system.active.durationSmudge = system.params.duration.base * (1 + _.round(_.random(-system.params.duration.smudge, system.params.duration.smudge)))
+      },
+      scheduleEvent (event, lineIndex) {
+        Tone.Transport.schedule((time) => {
+          let line = 'line' + lineIndex
+          this.$parent[line].synth.triggerAttackRelease(event.frequency, event.duration, time + event.start, 0.75)
+          // console.log('frequency:', event.frequency, 'duration:', event.duration, 'start:', event.start+time)
+
+        })
+      }
+    },
+    mounted() {
+      Tone.Transport.start()
+
+      setInterval(() => {
+        this.cacheBreak = Math.random()
+      }, 1000)
+    },
+    beforeDestroy() {
+      Tone.Transport.stop()
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  button {
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+  }
+
+  img {
+    height: 100vh;
+    width: 100vw;
+  }
+
+  #debug {
+    position: absolute;
+    top: 0;
+    z-index: 999999;
+  }
+</style>
